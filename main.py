996fe5803797fbf4ca1334c652376ebf68cc19e1
Antoine COURANT <147186020+CodingMakotoEpitech@users.noreply.github.com>
2024-03-20 23:22:52 +0100

    Added PDF Subject and Main Python File

---
+++ main.py
@@ -0,0 +1,35 @@
+choix = {"...", "...", "..."}
+
+print("...")
+j1 = input("...") # Récupère ce que l'utilisateur a écrit
+print("...")
+j2 = input("...")
+
+if ... not in choix or ... not in ...: # L'utilisateur a entré un choix invalide
+  print("...")
+else:
+  print("Le joueur 1 à choisi:", ...)
+  print("Le joueur 2 à choisi:", ...)
+  
+  if j1 == "Pierre": # Si l'utilisateur 1 a choisi "Pierre"
+    if j2 == "...":
+      print("Le joueur 1 à gagné !")
+    if j2 == "Feuille":
+      print("Le joueur 2 à gagné !")
+    elif j2 == "...":
+      print("...")
+      
+  elif ... == "Feuille":
+    if j2 == "Ciseaux":
+      print("Le joueur 2 à gagné !")
+    ... == ...:
+      print("...")
+    elif j2 == "...":
+      print("Le joueur 1 à gagné !")
+    
+  elif ...:
+    if j2 == "Ciseaux":
+      print("...")
+    if j2 == "Feuille":
+      ...
+    ...
